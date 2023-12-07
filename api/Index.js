bf2fe710d60a5508ac9608f353ebf3d59fde70ee
Delesoo <strongtoka@gmail.com>
2023-12-07 21:34:23 +0400

    Login and register form

---
+++ api/Index.js
@@ -0,0 +1,20 @@
+const express = require ('express');
+const cors = require ('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:5173',
+}));
+
+app.get('/test', (req,res) => {
+    res.json('test ok');
+});
+
+app.post('/register', (req,res) => {
+    const {name,email,password} = req.body;
+    res.json({name,email,password});
+});
+
+app.listen(4000);
