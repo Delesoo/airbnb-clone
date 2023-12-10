62bc581c51ff0e43cb9c71ec8988a7343f5a6be6
Delesoo <strongtoka@gmail.com>
2023-12-10 13:51:32 +0400

    imported mongoose database

--- api/Index.js
+++ api/Index.js
@@ -1,6 +1,12 @@
 const express = require ('express');
 const cors = require ('cors');
+const { default: mongoose } = require('mongoose');
+const bcrypt = require('bcryptjs');
+const User = require('./models/User.js');
+require('dotenv').config()
 const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 app.use(express.json());
 app.use(cors({
@@ -8,13 +14,35 @@ app.use(cors({
     origin: 'http://localhost:5173',
 }));
 
+mongoose.connect(process.env.MONGO_URL);
+
 app.get('/test', (req,res) => {
     res.json('test ok');
 });
 
-app.post('/register', (req,res) => {
+app.post('/register', async (req,res) => {
     const {name,email,password} = req.body;
-    res.json({name,email,password});
+    try {
+        const userDoc = await User.create ({
+            name,
+            email,
+            password:bcrypt.hashSync(password, bcryptSalt),
+        });
+        res.json(userDoc);
+    } catch (e) {
+        res.status(422).json(e);
+    }
+    
+});
+
+app.post('/login', async (req,res) => {
+   const {email,password} = req.body;
+   const userDoc = await User.findOne({email});
+   if (userDoc) {
+    res.json('found');
+   } else {
+    res.json('not found');
+   }
 });
 
 app.listen(4000);
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -6,13 +6,18 @@ export default function RegisterPage() {
     const [name,setName] = useState('');
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
-    function registerUser(ev) {
+    async function registerUser(ev) {
         ev.preventDefault();
-        axios.post('/register', {
+        try {
+            await axios.post('/register', {
             name,
             email,
             password,
         });
+        alert('Registration successful');
+        } catch (e) { 
+            alert("Registration failed. we get em' next time");
+         }    
     }
     return (
         <div className="mt-4 grow flex items-center justify-around">
