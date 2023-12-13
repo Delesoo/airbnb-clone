d3935e8da2ffd353dafb4f017081f96cacd38afe
Delesoo <strongtoka@gmail.com>
2023-12-13 14:11:00 +0400

    account page

--- api/Index.js
+++ api/Index.js
@@ -74,4 +74,8 @@ app.get('/profile', (req,res) => {
     }
 })
 
+app.post('/logout', (req,res) => {
+    res.cookie('token', '').json(true);
+});
+
 app.listen(4000);
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import RegisterPage from './pages/RegisterPage';
 import axios from 'axios';
 import { UserContextProvider } from './UserContext';
 import { useEffect } from 'react';
+import AccountPage from './pages/AccountPage';
 
 axios.defaults.baseURL = 'http://localhost:4000'
 axios.defaults.withCredentials = true;
@@ -19,6 +20,7 @@ function App() {
         <Route index element={<IndexPage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
+        <Route path='/account/:subpage?' element={<AccountPage />} />
       </Route>
     </Routes>
   </UserContextProvider>
