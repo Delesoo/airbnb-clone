e24bc261cde445ecef1551e4e159c132850fa0a1
Delesoo <strongtoka@gmail.com>
2023-12-23 16:35:50 +0400

    editing existing places

--- client/src/pages/PlacesFormPage.jsx
+++ client/src/pages/PlacesFormPage.jsx
@@ -18,7 +18,21 @@ export default function PlacesFormPage() {
     const [maxGuests,setMaxGuests] = useState(1);
     const [redirect,setRedirect] = useState(false);
     useEffect(() => {
-
+        if (!id) {
+            return;
+        }
+        axios.get('/places/'+id).then(response => {
+            const {data} = response;
+            setTitle(data.title);
+            setAddress(data.address);
+            setAddedPhotos(data.photos);
+            setDescription(data.description);
+            setPerks(data.perks);
+            setExtraInfo(data.extraInfo);
+            setCheckIn(data.checkIn);
+            setCheckOut(data.checkOut);
+            setMaxGuests(data.maxGuests)
+        });
     }, [id]);
     function inputHeader(text) {
         return (
@@ -38,10 +52,20 @@ export default function PlacesFormPage() {
               </>
           );
       }
-      async function addNewPlace(ev) {
+      async function savePlace(ev) {
           ev.preventDefault();
-         await axios.post('/places', {title, address, addedPhotos, description, perks, extraInfo, checkIn, checkOut, maxGuests});
-        setRedirect(true);
+          const placeData = {
+            title, address, addedPhotos, description, perks, extraInfo, checkIn, checkOut, maxGuests
+          }
+          if (id) {
+            //update
+            await axios.put('/places/', {id, ...placeData});
+            setRedirect(true);
+          } else {
+            //new place
+            await axios.post('/places', placeData);
+            setRedirect(true);
+          }
       }
 
       if (redirect) {
@@ -51,7 +75,7 @@ export default function PlacesFormPage() {
     return (
         <div>
                 <AccountNav />
-                <form onSubmit={addNewPlace}>
+                <form onSubmit={savePlace}>
                     {preInput('Title', 'Title for your place, should be catchy')}
                     <input type="text" value={title} onChange={ev => setTitle(ev.target.value)} placeholder="title, for example: My lovely apt" />
                     {preInput('Address', 'Address to this place')}
